44a4f581ab8d37139ebb3387d3900b43315abeb0
Filip Janiszewski <janiszewski.computing@gmail.com>
2015-12-29 16:43:46 +0100

    Minor cleaning action

--- ShortestPathWithMagic.cpp
+++ ShortestPathWithMagic.cpp
@@ -12,23 +12,42 @@ class ShortestPathWithMagic
 {
 	vector<bool> vis;
 	double r{1e9};
+	stack<int> path;
 	void find_path(int f,int t,int k,double d)
 	{
+		path.push(f);
 		if(f==t){
+			//cout<<"@:"<<f<<","<<k<<","<<d<<endl;
 			r = min(d,r);
+			stack<int> p_path = path;
+			while(p_path.size()>0)
+			{
+				cout<<p_path.top()<<",";
+				p_path.pop();
+			}
+			cout<<" = "<<r<<endl;
+			path.pop();
 			return;
 		}
-		if(vis[f]==true) return;
-		if(d>r) return ;
+		if(vis[f]==true)
+		{
+			path.pop();
+			return;
+		}
+		if(d>r){
+			path.pop();
+		       	return ;
+		}
 		vis[f] = true;
 		for(int i{0};i<V;i++)
 		{
-			//cout<<"#:"<<f<<","<<t<<","<< mp[c][i]<<endl;
+			if(mp[f][i]==0) continue;
 			if(k>=1){
 				find_path(i,t,k-1, d + double(mp[f][i])*.5);
 			}
 			find_path(i,t,k, d + double(mp[f][i]));
 		}
+		path.pop();
 		vis[f] = false;
 	}
 public:
@@ -40,7 +59,9 @@ public:
 			for(int j{0};j<d[i].size();j++)
 			{
 				mp[i][j] = d[i][j]-'0';
+				cout<<d[i][j];
 			}
+			cout<<endl;
 		}
 		vis.resize(V+1,false);
 		find_path(0,1,k,0);
@@ -48,13 +69,14 @@ public:
 	}
 };
 
+
 int main()
 {
 	ShortestPathWithMagic p;
 	vector<string> s1 = {"094", "904", "440"};
 	vector<string> s2 = {"076237", "708937", "680641", "296059", "334508", "771980"};
 	vector<string> s3 = {"09247266124", "90123790803", "21068509704", "42607851294", "73870470851", "27584008427", "69057008220", "60910880035", "18728420068", "20095223606", "43441705860"};
-	cout<<p.getTime(s3,1)<<endl;
+	cout<<p.getTime(s2,2)<<endl;
 	//cout<<p.getTime(s2,1)<<endl;
 
 }
--- emailScramble.cpp
+++ emailScramble.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <mutex>
 #include <map>
 #include <string>
 #include <set>
@@ -15,27 +16,42 @@ using ll = long long;
 
 constexpr const char* email = "My surname + .computing@gmail.com";
 constexpr int size = 33;
+constexpr int cores = 8;
 
 int data[size][size];
 
 int max_path{0};
 
-//deque<int> path[4];
-//deque<int> best_path[4];
+//deque<int> path[cores];
+//deque<int> best_path[cores];
 
-int path[4][size];
-int best_path[4][size];
+int path[cores][size];
+int best_path[cores][size];
 int longest[size];
 
-vector<bool> vis[4];
-long mx_path[4];
-bool running[4];
-thread workers[4];
+vector<bool> vis[cores];
+long mx_path[cores];
+bool running[cores];
+thread workers[cores];
 
 atomic_int running_worker;
+ll dfs_call;
+ll path_found;
+ll path_found_last;
+mutex call_print_mutex;
+std::chrono::time_point<chrono::high_resolution_clock> start;
 
-long dijkstra(int s,int d,int dist,int th_index,int& st_index)
+long dfs(int s,int d,int dist,int th_index,int& st_index)
 {
+	++dfs_call;
+	if(dfs_call%1000000000==0){
+		if(call_print_mutex.try_lock()==true){
+			auto cur_time = chrono::high_resolution_clock::now();
+			ll execution_time = chrono::duration_cast<chrono::seconds>(cur_time-start).count();
+			cout<<"Path found:" <<path_found<<" ("<<(path_found-path_found_last)/execution_time<<" p/s), still counting. time "<<execution_time<<"s"<<endl;
+			call_print_mutex.unlock();
+		}
+	}
 	path[th_index][st_index] = s;
 	long r{0};
 	if(s==d){
@@ -45,7 +61,8 @@ long dijkstra(int s,int d,int dist,int th_index,int& st_index)
 				best_path[th_index][i] = path[th_index][i];
 		}
 		--st_index;
-		return 0;
+		++path_found;
+		return mx_path[th_index];
 	}
 	vis[th_index][s] = true;
 	for(int i{0};i<size;i++)
@@ -54,7 +71,7 @@ long dijkstra(int s,int d,int dist,int th_index,int& st_index)
 		{
 			if(s==i || vis[th_index][i] == true) continue;
 			++st_index;
-			long t = data[s][i] + dijkstra(i,d,dist + data[s][i],th_index,st_index);
+			long t = data[s][i] + dfs(i,d,dist + data[s][i],th_index,st_index);
 			if(t > r)
 			{
 				r = t;
@@ -69,7 +86,7 @@ long dijkstra(int s,int d,int dist,int th_index,int& st_index)
 void execute(int s,int d,int dist,int th_index)
 {
 	int stack_index{1};
-	dijkstra(s,d,dist,th_index,stack_index);
+	dfs(s,d,dist,th_index,stack_index);
 	//Saving results.
 	if(mx_path[th_index]>max_path){
 		max_path = mx_path[th_index];
@@ -82,13 +99,14 @@ void execute(int s,int d,int dist,int th_index)
 
 long launch_platform(long s, int d)
 {
-	for(int i{0};i<4;i++)
+	for(int i{0};i<cores;i++)
 	{
 		vis[i].resize(size);
 		vis[i][0] = true;
 		mx_path[i] = 0;
 		running[i] = false;
 	}
+	start = chrono::high_resolution_clock::now();
 	for(int i{0};i<size;)
 	{
 		if(data[0][i]==0){
@@ -96,7 +114,7 @@ long launch_platform(long s, int d)
 			continue;
 		}
 		bool ok{false};
-		for(int r{0};r<4;r++)
+		for(int r{0};r<cores;r++)
 		{
 			if(running[r]==false)
 			{
@@ -123,10 +141,12 @@ long launch_platform(long s, int d)
 		}
 	}
 	cout<<"Joining all\n";
-	for(int i{0};i<4;i++)
+	for(int i{0};i<cores;i++)
 	{
 		if(workers[i].joinable()) workers[i].join();
 	}
+	cout<<"DFS call: "<<dfs_call<<endl;
+	cout<<"Number of path found: "<<path_found<<endl;
 	return max_path;
 }
 
@@ -140,21 +160,47 @@ void create_graph()
 		for(int j{0};j<size;j++)
 		{
 			if(i==j || (i==0 && j==size-1)) continue;
-			if(generate_canonical<double,10>(eng)<=.46){
+			if(generate_canonical<double,10>(eng)<=.17){
 				data[i][j] = abs(dist(eng));
 			}
 		}
 	}
+	/*
+	vector<vector<int>> matrix = {
+	{0,0,0,12,0,0,2,3,1,17,46,0,0,9,6,0,0,24,0},
+	{0,0,23,0,0,28,0,0,0,0,0,0,0,13,0,1,0,3,5},
+	{6,0,0,16,0,0,0,0,4,0,0,0,0,28,5,16,0,21,0},
+	{19,14,0,0,0,6,0,1,0,10,1,5,8,0,0,0,14,0,4},
+	{0,0,9,0,0,0,0,7,19,0,0,0,0,0,0,0,0,20,5},
+	{0,1,0,0,0,0,0,0,5,5,0,23,39,16,0,0,0,0,9},
+	{0,0,0,40,1,7,0,0,0,0,20,13,0,14,0,10,0,0,0},
+	{0,16,10,5,0,0,22,0,0,3,0,0,0,8,14,0,0,1,0},
+	{0,0,0,0,0,16,11,13,0,0,0,10,0,18,20,0,12,14,8},
+	{45,0,0,19,14,16,0,0,0,0,0,0,44,0,0,6,9,17,25},
+	{0,0,31,0,22,0,15,0,0,19,0,37,6,0,0,42,0,0,0},
+	{0,0,0,0,0,11,0,0,4,14,0,0,0,7,0,0,5,27,0},
+	{0,0,10,0,0,23,0,0,0,0,26,0,0,1,0,11,0,0,12},
+	{0,0,0,0,0,24,0,0,0,18,19,17,0,0,22,8,44,0,0},
+	{0,4,35,0,10,0,0,20,0,22,0,0,34,35,0,20,1,0,5},
+	{0,0,0,5,20,0,0,0,0,0,0,9,2,0,0,0,27,43,14},
+	{0,3,12,25,0,0,0,0,7,0,0,19,0,0,1,0,0,18,0},
+	{31,24,0,0,0,0,15,0,1,32,11,0,32,0,0,0,38,0,0},
+	{8,0,0,2,0,0,0,0,0,0,15,0,7,0,0,0,12,0,0}
+	};
+	//0,10,15,4,8,14,7,6,3,1,5,12,2,13,16,11,17,9,18,
+	*/
 	//Print the graph
 	for(int i{0};i<size;i++)
 	{
+		cout<<"{";
 		for(int j{0};j<size;j++)
 		{
-			cout<<data[i][j]<<",";
+	//		data[i][j] = matrix[i][j];
+			cout<<data[i][j]<<(j<size-1?",":"");
 		}
-		cout<<endl;
+		cout<<"}"<<endl;
 	}
-//	long r = dijkstra(0,size-1,0);
+//	long r = dfs(0,size-1,0);
 	auto start = chrono::high_resolution_clock::now();
 	long r= launch_platform(0,size-1);
 	cout<<"Longest: "<<r<<endl;
