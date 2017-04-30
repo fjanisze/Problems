148a91b0c11025d35ca0251756621e3b2ba42e39
Filip Janiszewski <filip.7an@gmail.com>
2017-04-30 19:15:56 +0200

    Fixed bug

--- AmpleSyrup.cpp
+++ AmpleSyrup.cpp
@@ -4,65 +4,57 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
+#include <set>
 using namespace std;
 
 constexpr long double pi = 3.141592653589793238462643383279502884;
 
 struct pan
 {
-    long double r;
-    long double t;
-    long double s;
-    pan(long double a,long double b ,long double c) :
-        r{ a }, s{ b }, t{ c } {};
-    bool operator<(const pan& o) {
-	return r > o.r;
-    }
+    long r;
+    long double tot_h;
+    long double tot_area;
 };
 
-std::vector< pan > data;
+pan data[ 1001 ];
+
 long sz;
 long n,k;
 
-long double solve( long idx, long ck )
-{
-    if( ck > k ) {
-	return 0;
-    }
-    long double best{ 0 };
-    while ( idx < sz )
-    {
-	long double t = solve( idx + 1, ck + 1 ) + data[ idx ].s;
-	if( ck > 1 ) {
-	    t -= data[ idx ].t;
-	}
-	if( t > best ) {
-	    best = t;
-	}
-	++idx;
-    }
-    return best;
-}
-
 int main()
 {
-    long tt;
-    cin >> tt;
-    for( long t{1} ; t <= tt ; ++t )
+    long tc;
+    cin >> tc;
+    for( long t{ 1 } ; t <= tc; ++t )
     {
-	data.clear();
-	malus = 0;
 	cin >> n >> k;
-	while( n-- )
-	{
-	    long double r,h;
+	long r,h;
+	for( long i { 0 } ; i < n ; ++i) {
 	    cin >> r >> h;
-	    data.push_back( pan( r, 2 * pi * r * h + r * r * pi , r * r * pi ) );
+	    pan new_pan;
+	    new_pan.r = r;
+	    new_pan.tot_h = 2 * pi * h * r;
+	    new_pan.tot_area = r * r * pi + new_pan.tot_h;
+	    data[i] = new_pan;
 	}
-	std::sort(data.begin(),data.end());
-
-	sz = data.size();
-
-	std::cout<<"Case #"<<t<<": "<<std::setprecision(20)<<solve( 0, 1 )<<std::endl;
+	std::sort(data,data + n,[](const pan& a,const pan& b){ return a.r < b.r; });
+	//Sorted by r, increasing order
+	long double res{ 0 };
+	long double out{ 0 };
+	for( long i { k - 1 } ; i < n ; ++i )
+	{
+	    res = data[ i ].tot_area;
+	    std::vector<pan> t_set;
+	    for( long j { i - 1 } ; j >= 0 ; --j ) {
+		t_set.emplace_back( data[ j ] );
+	    }
+	    std::sort(t_set.begin(),t_set.end(),[](const pan& a,const pan& b){ return a.tot_h > b.tot_h; });
+	    for( long j {0} ; j < k - 1 ; ++j)
+	    {
+		res += t_set[j].tot_h;
+	    }
+	    out = max( out , res );
+	}
+	std::cout<<"Case #"<<t<<": "<<std::setprecision(20)<<out<<std::endl;
     }
 }
